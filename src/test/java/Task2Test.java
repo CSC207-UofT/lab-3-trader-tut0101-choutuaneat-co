497841644528f2d3716afc1907664496d6bcfc51
Yuxuan-Yang-Maggie <yuxuanmaggie.yang@mail.utoronto.ca>
2021-09-27 11:44:57 -0400

    I've written tests now.

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Task2Test {
+    Task2 H;
+
+    @Before
+    public void setUp() throws Exception {
+        H = new Task2();
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals(null, H.sound());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetMaxSpeed() {
+        assertEquals(0, H.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(0, H.getPrice());
+    }
 }
